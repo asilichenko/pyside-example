0b6771a4560c0b4b49912c660d515f2faccf4759
Oleksii Sylichenko <a.silichenko@gmail.com>
2026-05-15 17:16:42 +0300

    update app.py

--- pyside_example/app.py
+++ pyside_example/app.py
@@ -1,4 +1,5 @@
 import logging
+import sys
 
 from PySide6.QtWidgets import QApplication
 
@@ -7,16 +8,48 @@ from ui.main_window import MainWindow
 logger = logging.getLogger(__name__)
 
 
-class PySide6Example:
+def run() -> None:
+    logging_config()
+    logger.info("Starting")
 
-    def run(self):
-        app = QApplication([])
+    app = QApplication(sys.argv)
+    app.aboutToQuit.connect(on_shutdown)
 
-        try:
-            main_wnd = MainWindow()
-            main_wnd.show()
-        except Exception:
-            logger.error("Failed to start")
-            raise
+    try:
+        main_wnd = MainWindow()
+    except Exception:
+        logger.exception("Failed to start")  # автоматично додає traceback
+        raise
 
-        app.exec_()
+    main_wnd.show()
+    sys.exit(app.exec())  # блокуючий, код після цього рядка вже не виконається
+
+
+def on_shutdown() -> None:
+    logger.info("Shutdown")
+
+
+def logging_config(level=logging.DEBUG):
+    root_logger = logging.getLogger()
+    root_logger.setLevel(level)
+
+    if root_logger.hasHandlers():
+        root_logger.handlers.clear()
+
+    date_format: str = '%Y-%m-%d %H:%M:%S'
+    log_format: str = '%(asctime)s [%(levelname)s] %(name)s[%(funcName)s]: %(message)s'
+    formatter = logging.Formatter(log_format, date_format)
+
+    console_handler = logging.StreamHandler()
+    console_handler.setFormatter(formatter)
+    root_logger.addHandler(console_handler)
+
+    # err_log_handler: logging.FileHandler = RotatingFileHandler("logs/error.log", maxBytes=1_000_000, backupCount=3)
+    # err_log_handler.setLevel(logging.ERROR)
+    # err_log_handler.setFormatter(formatter)
+    # root_logger.addHandler(err_log_handler)
+    #
+    # debug_log_handler: logging.FileHandler = RotatingFileHandler("logs/debug.log", maxBytes=1_000_000, backupCount=3)
+    # debug_log_handler.setLevel(logging.DEBUG)
+    # debug_log_handler.setFormatter(formatter)
+    # root_logger.addHandler(debug_log_handler)
